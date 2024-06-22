836beffa10ff17621974bbc3ef8e5fba461eafa4
nazaryan0909 <alenn0717@gmail.com>
2024-06-22 11:26:49 -0700

    avelacrel em B varjutyun

--- main.js
+++ main.js
@@ -4,19 +4,19 @@
 // 9. Արտածել տրված [a,b] միջակայքին պատկանող դրական ամբողջ սիմետրիկ թվերի միջին թվաբանականը: 
 // 10. Արտածել տրված n թիվը չգերազանցող բոլոր այն բնական թվերը, որոնք առանց մնացորդի բաժանվում են իրենց թվանշանների գումարի վրա:
 
-function repeat(arr){
-    let arr1 = [];
-    for(let i = 0; i < arr.length; i++){
-     if(arr1.includes(arr[i]) == true){
-         return true
-     }else {
-        arr1.push(arr[i])
-     }
-    }
-    return false
+// function repeat(arr){
+//     let arr1 = [];
+//     for(let i = 0; i < arr.length; i++){
+//      if(arr1.includes(arr[i]) == true){
+//          return true
+//      }else {
+//         arr1.push(arr[i])
+//      }
+//     }
+//     return false
    
-}
-console.log(repeat([8, 9, 5, 8, 2]))
+// }
+// console.log(repeat([8, 9, 5, 8, 2]))
 
 // բ) այն տարրերի քանակը, որոնք զանգվածում հանդիպում են ճիշտ 1 անգամ: 
  
@@ -32,3 +32,22 @@ console.log(repeat([8, 9, 5, 8, 2]))
 // }
 // console.log(oneOnly([2, 8, 1, 2, 9, 4]))
 
+function f(arr) {
+    let obj = {}
+    let count = 0;
+    for (let i = 0; i = arr.length; i++){
+        if (obj[arr[i]] === undefined) {
+          obj[arr[i]] = 1
+        }else {
+            obj[arr[i]]++
+        }
+    }
+    for (let key in obj){
+        if(obj[key] === 1){
+            count++
+        }
+    }
+    return count
+}
+
+console.log(f([2,3,4,5,2,4,5,7,8]))
